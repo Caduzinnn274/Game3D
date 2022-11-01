689ab46c22401c3a068827aff1c0a2243e84e8f9
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2022-11-01 19:40:39 -0300

    Create RayCastScript.cs

---
+++ RayCastScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastScrit : MonoBehaviour
+{
+
+    public float distanciaAlvo;
+    public GameObject objArrasta, objPega;
+    RaycastHit hit;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.frameCount % 5 == 0)
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 5, Color.red);
+
+            if(Physics.SphereCast(transform.position, 0.1f, transform.TransformDirection(Vector3.forward), out hit, 5))
+            {
+                distanciaAlvo = hit.distance;
+
+                if(hit.transform.gameObject.tag == "objArrasta")
+                {
+                    objArrasta = hit.transform.gameObject;
+                }
+                if(hit.transform.gameObject.tag == "objPega")
+                {
+                    objPega = hit.transform.gameObject;
+                }
+                        
+            }
+        }
+        else
+        {
+            objPega = null;
+            objArrasta = null;
+        }
+
+        
+    }
+}
